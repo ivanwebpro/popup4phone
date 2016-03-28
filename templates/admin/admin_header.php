d6c730d2815ee1686c01643bbfb81691bcc6cfca
unknown <ivan@ivanpro.com>
2016-03-28 13:09:21 +0700

    review help page, details for inline button

--- templates/admin/admin_header.php
+++ templates/admin/admin_header.php
@@ -1,6 +1,7 @@
 <?php if (!$this->root()->is_premium_installed()) { ?>
 
-<div style='flo1at: right'><a href='http://popup4phone.com/premium/' target='_blank'><b><?php _e("Want to have more leads? Click to get Popup4Phone Premium version", 'popup4phone');?></b></a></div>
+<a href='http://popup4phone.com/premium/' target='_blank'><b><?php _e("Want to have more leads? Click to get Popup4Phone Premium version", 'popup4phone');?></b></a>
+<br>
 
 <?php
 	}
--- templates/admin/help.tpl.php
+++ templates/admin/help.tpl.php
@@ -1,26 +1,35 @@
 <div class="wrap popup4phone_list_pointed">
-	<?php include 'admin_header.php'; ?>
+
 	<h2><?php echo "Popup4Phone: ".__( 'Help', 'popup4phone' );?></h2>
 
 
 	<div style='float: right; width: 50%; border: 1px solid green; padding: 5px;'>
-	<p><b><?php printf( __( 'If you like Popup4Phone please leave a %s rating.', 'popup4phone' ), $this->html_a('&#9733;&#9733;&#9733;&#9733;&#9733;', "https://wordpress.org/support/view/plugin-reviews/popup4phone?filter=5#postform", array('target' => "_blank", 'style' => "text-decoration:none") ) );
-	print "<br>";
+		<?php include 'admin_header.php'; ?>
+
+	<?php
+		$rating_url = "https://wordpress.org/support/view/plugin-reviews/popup4phone#postform";
+	?>
+	<p><b><?php printf( __( 'If you like Popup4Phone please leave a %s rating', 'popup4phone' ), $this->html_a('&#9733;&#9733;&#9733;&#9733;&#9733;', $rating_url, array('target' => "_blank", 'style' => "text-decoration:none") ) );
+	echo ":<br>";
+	echo $this->html_a($rating_url, $rating_url, array('target' => "_blank", 'style' => "text-decoration:none") );
+	echo "<br>";
 	_e("Thank you very much in advance!", 'popup4phone');
 	?></b></p>
 
 	<p><b><?php
 		$url = "http://popup4phone.com/contact/";
 
-		printf( __( 'If you have any problems - please contact me: %s', 'popup4phone' ), $this->html_a($url, $url, array('target' => "_blank", 'style' => "text-decoration:none")));
+		///printf( __( 'If you have any problems - please contact me: %s', 'popup4phone' ), $this->html_a($url, $url, array('target' => "_blank", 'style' => "text-decoration:none")));
+		_e( 'Support', 'popup4phone' );
+		print ": ".$this->html_a($url, $url, array('target' => "_blank", 'style' => "text-decoration:none"));
 
 
 	?></b></p>
 
 	<p><b><?php
-		$url = "https://translate.wordpress.org/projects/wp-plugins/popup4phone";
+		//$url = "https://translate.wordpress.org/projects/wp-plugins/popup4phone";
 
-		printf( __( 'Help to translate plugin on your language: %s', 'popup4phone' ), $this->html_a($url, $url, array('target' => "_blank", 'style' => "text-decoration:none")));
+		//printf( ( 'Help to translate plugin on your language: %s', 'popup4phone' ), $this->html_a($url, $url, array('target' => "_blank", 'style' => "text-decoration:none")));
 
 
 	?></b></p>
@@ -30,8 +39,8 @@
 
 	<p><?php _e( 'There are three ways to use Popup4Phone plugin', 'popup4phone' );?>:</p>
 	<ul>
-		<li><?php _e( 'Auto popup with some delay after loading of the page', 'popup4phone' );?></li>
-		<li><?php _e( 'Popover button, where the user can click and open the form', 'popup4phone' );?></li>
+		<li><?php _e( 'Auto popup with delay after loading of the page', 'popup4phone' );?></li>
+		<li><?php _e( 'Popover/inline button, where the user can click and open the form', 'popup4phone' );?></li>
 		<li><?php _e( 'Shortcode/inline form', 'popup4phone');?></li>
 	</ul>
 
@@ -39,9 +48,14 @@
 	<ul>
 		<li><b>[popup4phone_inline_form_no_popup]</b> &ndash; <?php _e( 'show inline form and lock auto popup form', 'popup4phone' );?></li>
 		<li><b>[popup4phone_inline_form]</b> &ndash; <?php _e( 'show inline form, NOT lock auto popup form', 'popup4phone' );?></li>
+		<li><b> [popup4phone_button_inline]</b> &ndash; <?php _e( 'inline button for open form', 'popup4phone' );?></li>
 	</ul>
 
-	<p><?php _e( 'Also I recommend to use some trigger email service (like Mandrill) for a better deliverability', 'popup4phone' );?>. Mandrill: <A href = 'https://mandrillapp.com' target='_blank'>https://mandrillapp.com</A>, <?php _e( 'plugin', 'popup4phone' )?>: <A href='https://wordpress.org/plugins/wpmandrill/' target='_blank'>https://wordpress.org/plugins/wpmandrill/</A></p>
+<?php
+/*
+	<p><?php ( 'Also I recommend to use some trigger email service (like Mandrill) for a better deliverability', 'popup4phone' );?>. Mandrill: <A href = 'https://mandrillapp.com' target='_blank'>https://mandrillapp.com</A>, <?php ( 'plugin', 'popup4phone' )?>: <A href='https://wordpress.org/plugins/wpmandrill/' target='_blank'>https://wordpress.org/plugins/wpmandrill/</A></p>*/
+
+?>
 
 	<h2><?php _e( 'Subscribe to mailing list and get priority support', 'popup4phone' ); ?>:</h2>
 
